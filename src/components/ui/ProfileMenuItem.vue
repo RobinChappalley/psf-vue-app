541406a7d82c612577d03ce06c9203385082d88c
Cedrine MacRempl <cedrine.tille@gmail.com>
2026-01-05 15:39:50 +0100

    Create new component

---
+++ src/components/ui/ProfileMenuItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <button class="menu-item" type="button" @click="$emit('click')">
+    <span class="label">
+      <slot />
+    </span>
+    <span class="chevron" aria-hidden="true">›</span>
+  </button>
+</template>
+
+<script setup>
+defineEmits(['click'])
+</script>
+
+<style scoped>
+.menu-item {
+  width: 100%;
+  padding: var(--sp-2) 0;
+  background: none;
+  border: none;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: var(--fs-body);
+  color: var(--c-text);
+  cursor: pointer;
+  border-bottom: 1px solid var(--c-border);
+}
+
+.menu-item:active {
+  opacity: 0.7;
+}
+
+.label {
+  text-align: left;
+}
+
+.chevron {
+  font-size: 1.25rem;
+  color: var(--c-text);
+  opacity: 0.6;
+}
+</style>
